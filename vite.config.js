6b940e2aa279ff4d0b4faecfb41a92ff83d9aeef
Mike <mikecsomma@gmail.com>
2023-02-05 12:47:15 -0300

    Modi conf de vite.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/js-vite-blackjack/'
+    base: '/js-vite-blackjack/', // Para que funcione en github pages. /js-vite-blackjack/ es el nombre del repo.
+    build: {
+        outDir: './docs' // Se modifica de ./dist a ./docs para que funcione en github pages. 
+      }
 });
